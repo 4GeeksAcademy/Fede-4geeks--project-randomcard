d936c762aa6d1862aa749fac5357c5b06eaf445c
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-03-11 09:07:35 +0000

    proyecto finalizado empezando desde ejemp profe

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
+
+function randomSelectionArray(value) {
+    let randomIndex = Math.floor(Math.random() * value.length)
+    return value[randomIndex]
+  }
+  
+function generateCard() {
+  let figures = randomSelectionArray(["♥", "♦", "♣", "♠"])
+  let numbers = randomSelectionArray(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"])
+  console.log({figures, numbers});
+  let simbolUpRender = document.getElementById("simbolUp");
+    simbolUpRender.innerHTML = `${figures}`
+  let simbolDownRender = document.getElementById("simbolDown");
+  simbolDownRender.innerHTML = `${figures}`
+  let numberRender = document.getElementById("numberCard");
+  numberRender.innerHTML = `${numbers}`
+  if (figures === "♥" || figures === "♦") {
+    simbolUpRender.style.color = "red";
+    simbolDownRender.style.color = "red";
+    numberRender.style.color = "red";
+  } else {
+    simbolUpRender.style.color = "black";
+    simbolDownRender.style.color = "black";
+    numberRender.style.color = "black";
+  }
+}
+
+function changeSize() {
+  let width = document.getElementById("width").value + "px";
+  let height = document.getElementById("height").value + "px";
+  let cardElement = document.getElementById("card");
+  cardElement.style.width = width;
+  cardElement.style.height = height;
+}
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("sizeButton").addEventListener('click', changeSize);
+  document.getElementById("cardButton").addEventListener('click', generateCard);
+  setInterval(generateCard, 1000);
 };
